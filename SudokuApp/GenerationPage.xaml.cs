9058627b9c9f9b23ccc011d2e4063981ab6de434
Rem <run@remware.net>
2016-09-25 23:13:21 +0300

    Initial settings for Sudoku generation

---
+++ SudokuApp/GenerationPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace SudokuApp
+{
+    /// <summary>
+    /// Interaction logic for GenerationPage.xaml
+    /// </summary>
+    public partial class GenerationPage : Page
+    {
+        public GenerationPage()
+        {
+            InitializeComponent();
+        }
+
+        // Custom constructor to pass generator data
+        public GenerationPage(object data) : this()
+        {
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+    }
+}
--- SudokuApp/SudokuHome.xaml.cs
+++ SudokuApp/SudokuHome.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SudokuApp
 {
@@ -23,6 +11,14 @@ namespace SudokuApp
         public SudokuHome()
         {
             InitializeComponent();
+        }
+
+        private void Go_Generate(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            GenerationPage generateSudokuPage = new GenerationPage(this.complexityListBox.SelectedItem);
+            this.NavigationService.Navigate(generateSudokuPage);
+
         }
     }
 }
