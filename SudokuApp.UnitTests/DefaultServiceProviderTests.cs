d3584c003bc0d67c29fc05ef4a8f2fb0d191257d
Rem <run@remware.net>
2016-10-05 02:23:14 +0300

    Generic and negative test cases

--- SudokuApp.UnitTests/DefaultServiceProviderTests.cs
+++ SudokuApp.UnitTests/DefaultServiceProviderTests.cs
@@ -18,6 +18,28 @@ namespace SudokuApp.UnitTests
             Assert.AreEqual(problemsAreListedIn.Capacity, ExpectedProblemList.Capacity);
         }
 
+        [TestMethod]
+        public void GivenViewModel_WhenInitialized_ProblemsAreAvailable()
+        {
+            // Arrange
+            var whenInitialized = new SudokuProblemViewModel(GivenDefaultServiceProvider());
+            // Act
+            var problemsAreListedIn = whenInitialized.Service.GetProblems();
+            // Assert
+            Assert.AreEqual(problemsAreListedIn.Capacity, ExpectedProblemList.Capacity);
+        }
+
+        [TestMethod]
+        public void GivenViewModel_WhenNullService_ProblemsAreAvailable()
+        {
+            // Arrange
+            var whenNullService = new SudokuProblemViewModel(null);
+            // Act
+            var problemsAreListedIn = whenNullService.Service.GetProblems();
+            // Assert
+            Assert.AreEqual(problemsAreListedIn.Capacity, ExpectedProblemList.Capacity);
+        }
+
         private IService GivenDefaultServiceProvider()
         {
             return new DefaultServiceProvider();
